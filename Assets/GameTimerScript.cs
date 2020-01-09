d943fe1e4d4d0c3a66cf83b8a56e657e2d966ce4
CalenMac <calentmac@gmail.com>
2020-01-09 16:58:51 -0500

    Timer Added
    Timer added and instruction added

---
+++ Assets/GameTimerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameTimerScript : MonoBehaviour
+{
+    public Text gameTimerText;
+    float gameTimer=0f;    // Start is called before the first frame update
+   
+
+        // Update is called once per frame
+    void Update()
+    {
+        gameTimer += Time.deltaTime;
+        int seconds = (int)(gameTimer % 60);
+        int minutes = (int)(gameTimer / 60) % 60;
+
+        string timerString = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        gameTimerText.text = timerString;
+    }
+}
